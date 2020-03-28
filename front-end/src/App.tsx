9b8f0e87d4f19a26f397f86566ff9d99da227e57
Andrew Dimmer <contactandrewd@gmail.com>
2020-03-28 14:39:34 -0400

    feat(front-end): Added Page Navigation System

---
+++ front-end/src/App.tsx
@@ -0,0 +1,82 @@
+import React, { Fragment } from "react";
+import NavBar from "./Components/Layouts/NavBar";
+import { getPageTitle, getPageComponent } from "./Components/Pages";
+import NotificationBar, {
+  NotificationMessage
+} from "./Components/Misc/Notifications";
+import { firebaseApp } from "./Scripts/FirebaseConfig";
+import { styles } from "./Styles";
+import { Container } from "@material-ui/core";
+
+const App: React.FunctionComponent = () => {
+  const [currentUser, setCurrentUser] = React.useState<firebase.User | null>(
+    null
+  );
+  const [pageKey, setPageKey] = React.useState<string>("home");
+  const [notification, setNotification] = React.useState<NotificationMessage>({
+    type: "info",
+    message: "",
+    open: false
+  });
+  const [reloadUserData, setReloadUserData] = React.useState<boolean>(true);
+
+  const handleChangePage = (pageKey: string) => {
+    setPageKey(pageKey);
+  };
+
+  const handleUpdateNotification = (
+    notificationMessage: NotificationMessage
+  ) => {
+    setNotification(notificationMessage);
+  };
+
+  const forceReloadUserData = () => {
+    setReloadUserData(true);
+  };
+
+  const loadUserData = () => {
+    if (reloadUserData) {
+      const oneTimeLoadListener = firebaseApp
+        .auth()
+        .onAuthStateChanged(user => {
+          console.log("State change listener");
+          console.log(user);
+          setCurrentUser(user);
+          oneTimeLoadListener(); // Removes the listener after it runs
+        });
+      setReloadUserData(false);
+    }
+  };
+
+  loadUserData();
+
+  const classes = styles();
+  const PageContent = getPageComponent(pageKey);
+
+  return (
+    <Fragment>
+      <NavBar
+        currentUser={currentUser}
+        pageTitle={getPageTitle(pageKey)}
+        handleChangePage={handleChangePage}
+        classes={classes}
+      />
+      <NotificationBar
+        type={notification.type}
+        message={notification.message}
+        open={notification.open}
+        update={handleUpdateNotification}
+      />
+      <Container>
+        <PageContent
+          handleUpdateNotification={handleUpdateNotification}
+          setPageKey={setPageKey}
+          forceReloadUserData={forceReloadUserData}
+          classes={classes}
+        />
+      </Container>
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ front-end/src/Components/Pages/index.ts
@@ -0,0 +1,97 @@
+import { SvgIconTypeMap } from "@material-ui/core";
+import { OverridableComponent } from "@material-ui/core/OverridableComponent";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import HomeIcon from "@material-ui/icons/Home";
+import { NotificationMessage } from "../Misc/Notifications";
+import ErrorPage from "./ErrorPage";
+import HomePage from "./Home";
+import LoginPage from "./LoginPage";
+import LogoutPage from "./LogoutPage";
+import ProfilePage from "./ProfilePage";
+
+export declare interface PageProps {
+  handleUpdateNotification: (notificationMessage: NotificationMessage) => void;
+  setPageKey: (pageKey: string) => void;
+  forceReloadUserData: () => void;
+  classes: any;
+}
+
+export declare interface PageListForMenuItem {
+  key: string;
+  menuLabel: string;
+  menuIcon: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
+  displaySignedIn: boolean;
+  displaySignedOut: boolean;
+}
+
+declare interface PageListItem extends PageListForMenuItem {
+  title: string;
+  component: React.FunctionComponent<PageProps>;
+}
+
+const pageList: PageListItem[] = [
+  {
+    key: "home",
+    title: "",
+    menuLabel: "Home",
+    menuIcon: HomeIcon,
+    displaySignedIn: true,
+    displaySignedOut: true,
+    component: HomePage
+  },
+  {
+    key: "login",
+    title: "",
+    menuLabel: "Login",
+    menuIcon: ExitToAppIcon,
+    displaySignedIn: false,
+    displaySignedOut: true,
+    component: LoginPage
+  },
+  {
+    key: "profile",
+    title: "Profile",
+    menuLabel: "Profile",
+    menuIcon: AccountCircleIcon,
+    displaySignedIn: true,
+    displaySignedOut: false,
+    component: ProfilePage
+  },
+  {
+    key: "logout",
+    title: "",
+    menuLabel: "Logout",
+    menuIcon: ExitToAppIcon,
+    displaySignedIn: true,
+    displaySignedOut: false,
+    component: LogoutPage
+  }
+];
+
+const pageObject = pageList.reduce((pageObject, pageListItem) => {
+  pageObject[pageListItem.key] = pageListItem;
+  return pageObject;
+}, {} as { [key: string]: PageListItem });
+
+export const getPageComponent = (
+  key: string
+): React.FunctionComponent<PageProps> => {
+  if (pageObject[key]) {
+    return pageObject[key].component;
+  }
+  return ErrorPage;
+};
+
+export const getPageTitle = (key: string): string => {
+  if (pageObject[key]) {
+    return pageObject[key].title;
+  }
+  return "";
+};
+
+export const pageListForMenu: PageListForMenuItem[] = pageList.map(
+  ({ key, menuLabel, menuIcon, displaySignedIn, displaySignedOut }) => {
+    return { key, menuLabel, menuIcon, displaySignedIn, displaySignedOut };
+  }
+);
